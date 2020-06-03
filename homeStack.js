80fdcaeed03c6fc1f6761c4d02c39c03dc2e4c15
sanjeevmax6 <63261622+sanjeevmax6@users.noreply.github.com>
2020-06-03 13:25:09 +0530

    commit

---
+++ homeStack.js
@@ -0,0 +1,17 @@
+ import {createAppContainer} from 'react-navigation';
+ import {createStackNavigator} from 'react-navigation-stack';
+ import Home from '../screens/home'
+ import About from '../screens/about'
+
+ const screens = {
+     Home:{
+         screen: Home
+     },
+     About:{
+         screen: About
+     }
+ }
+
+const HomeStack = createStackNavigator({screens});
+
+ export default createAppContainer(HomeStack);
